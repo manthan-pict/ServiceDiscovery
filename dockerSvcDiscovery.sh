659857e66537b59740c83f7710cc922fb364bfc6
manthan-pict <manthanagrawal96@gmail.com>
2019-05-28 12:47:03 +0530

    pushed files to ec2/logs file

--- dockerSvcDiscovery.sh
+++ dockerSvcDiscovery.sh
@@ -1,7 +1,7 @@
-docker run -e CONSUL=consul -v /var/run/docker.sock:/var/run/docker.sock:ro --link=consul --name mysd1 -d my-sd-svc:1
+docker run -e CONSUL=consul -v /home/ec2-user/logs/demoApp:/logs -v /var/run/docker.sock:/var/run/docker.sock:ro --link=consul --name mysd1 -d my-sd-svc:1
 
 docker network connect bunit mysd1
 
-docker run -e CONSUL=consul -v /var/run/docker.sock:/var/run/docker.sock:ro --link=consul --name mysd2 -d my-sd-svc:1
+docker run -e CONSUL=consul -v /home/ec2-user/logs/demoApp:/logs -v /var/run/docker.sock:/var/run/docker.sock:ro --link=consul --name mysd2 -d my-sd-svc:1
 
 docker network connect bunit mysd2
